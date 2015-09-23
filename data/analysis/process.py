6e06bec1c2cb4d1db2215898ee958d2e5fbf6d6f
Séverin Lemaignan <severin@guakamole.org>
2015-09-23 09:30:41 +0100

    Different algo to compute with-me-ness, much closer to the paper

--- data/analysis/process.py
+++ data/analysis/process.py
@@ -90,49 +90,38 @@ plot("groundtruth", events_observed)
 #     With-me-ness
 ############################################################################
 
-def get_previous_observation_start(start, timestamps):
-    for idx, t in enumerate(timestamps):
-        if start < t:
-            yield timestamps[idx-1] if (idx-1) >= 0 else timestamps[0]
+from numpy import arange
+
+dt = 0.1 # seconds
+
+t_end = events_expected[events_expected.keys()[-1]][1]
+
+def find_interval(t, intervals):
+    for ts, te in intervals:
+        if ts <= t and te >= t:
+            return ts, te
+    return None, None
 
 def withmeness(observed, expected):
 
     total_time = 0.
     total_time_matching = 0.
 
-    # we iterate primarily on the *expected* targets of attention
-    for idx, t_expected_start in enumerate(expected.keys()):
+    expected_intervals = [(k, v[1]) for k,v in events_expected.items()]
+    observed_intervals = [(k, v[1]) for k,v in events_observed.items()]
 
-        targets, t_expected_end = expected[t_expected_start]
+    for t in arange(0, t_end, dt):
+            expected_start, _ = find_interval(t, expected_intervals)
+            observed_start, _ = find_interval(t, observed_intervals)
 
-        total_time += t_expected_end - t_expected_start
+            if expected_start and observed_start:
+                total_time += dt
 
-        # then, for each time period, we iterate over the actual observation,
-        # starting at the last observation *before* the considered period.
-        for t_obs_start in get_previous_observation_start(t_expected_start, observed.keys()):
+                target = events_observed[observed_start][0][0]
+                expected_targets = events_expected[expected_start][0]
 
-            target, t_obs_end = observed[t_obs_start]
-
-            # if the observation starts *after* the considered period, we move
-            # to the next period, ie the next 'expected' targets
-            if t_obs_start > t_expected_end: 
-                break
-
-            # if the observation ends *before* the considered period (it may be
-            # the case, since we start with the last observation which started
-            # before the period), we skip it.
-            if t_obs_end < t_expected_start:
-                continue
-
-            # if the observed target is one of the expected one, add the actual
-            # observation time *for this period* to total_time_matching
-            if target[0] in targets:
-                print("Match at %s!" % t_obs_start)
-                start = max(t_expected_start, t_obs_start)
-                end = min(t_expected_end, t_obs_end)
-                total_time_matching += end - start
-            else:
-                print("Mismatch at %s" % t_obs_start)
+                if target in expected_targets:
+                    total_time_matching += dt
 
     withmeness = total_time_matching/total_time
     print("Total time: %s sec, total time matching: %s sec, With-me-ness: %s" % (total_time, total_time_matching, withmeness))
